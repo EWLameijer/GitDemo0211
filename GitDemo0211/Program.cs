500ffe92b0be4a47e2aaf9f8d66e0491e11287a7
EWLameijer <eric_wubbo@hotmail.com>
2022-02-11 09:37:28 +0100

    First commit

---
+++ GitDemo0211/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitDemo0211
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
